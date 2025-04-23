67392794ab149e3e81fed0bcad0efca3246c820a
Shahriar Sabuz <mkssabuz@gmail.com>
2025-04-23 13:27:05 +0600

    RIQS Login Test Initial Commit

---
+++ cypress/e2e/RIQSLogin.cy.js
@@ -0,0 +1,14 @@
+describe('RIQS Login Test', () => {
+  it('Login with Valid Credentials & Verify Logo', () => {
+    // visit RIQS Login Page
+    cy.visit('https://praxismonitor.seliselocal.com/login')
+    // Input Email
+    cy.get('#mat-input-0').type("testuserriqs001@yopmail.com")
+    // Input Password
+    cy.get('#mat-input-1').type("T@1234u")
+    // Clik on Login Button
+    cy.get('button[aria-label="LOGIN"]').click()
+    // Logo Assertions
+    cy.get('img[alt="hrs dept demo 1"]').should('be.visible')
+  })
+})
